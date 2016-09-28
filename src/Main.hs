25576a8cc86e9ca15661c5d81b6d16a8272d6097
Divam <dfordivam@gmail.com>
2016-09-28 16:21:14 +0900

    Add average API and other changes

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,12 @@
+-- {-# LANGUAGE BangPatterns #-}
+
 module Main where
 
 import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as UV
 
 import System.Random
+import Data.Time
 
 type WTime = Double
 type TimingDataBlock = (WTime, WTime, WTime, WTime)
@@ -12,23 +15,57 @@ type TimingData = V.Vector TimingDataBlock
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  -- Uncomment these lines to create a db in first run
+  -- d <- createRandomData
+  -- writeFile "data.db" (show d)
 
+  str <- readFile "data.db"
+  let d = read str
+      bigd = V.concat (replicate 1000 d)
 
+  -- This should evaluate the whole 'bigd'
+  print $ addTimingData bigd
+
+  -- Profile just the average API
+  startTime <- getCurrentTime
+  print $ averageTimingData bigd
+  endTime <- getCurrentTime
+
+  print $ diffUTCTime endTime startTime
+
+-- Add all the data, useful just to evaluate the input completely
 addTimingData :: TimingData -> TimingDataBlock
 addTimingData timingData = addTuple addBins
   -- trace ("Timing data " ++ show accVec)
   where
     -- Split the work and then converge
     bins :: [TimingData]
-    bins = binTimingData 200 timingData
+    bins = binTimingData 20 timingData
 
+    -- This step can be done in parallel
     addBins :: [TimingDataBlock]
     addBins = fmap addTuple bins
 
-    addTuple :: (Foldable f) => f TimingDataBlock -> TimingDataBlock
-    addTuple = foldl1
-      (\(a1,b1,c1,d1) (a2,b2,c2,d2) -> (a1+a2,b1+b2,c1+c2,d1+d2))
+addTuple :: (Foldable f) => f TimingDataBlock -> TimingDataBlock
+addTuple = foldl1
+  (\(a1,b1,c1,d1) (a2,b2,c2,d2) -> (a1+a2,b1+b2,c1+c2,d1+d2))
+
+-- Take average of bins of data
+averageTimingData :: TimingData -> [TimingDataBlock]
+averageTimingData timingData = map (applyF (/(fromIntegral len))) addBins
+  where
+    -- Split the work and then converge
+    bins :: [TimingData]
+    bins = binTimingData 10 timingData
+
+    len = length (head bins)
+
+    -- This step can be done in parallel
+    addBins :: [TimingDataBlock]
+    addBins = fmap addTuple bins
+
+    applyF :: (WTime -> WTime) -> TimingDataBlock -> TimingDataBlock
+    applyF f (a1,b1,c1,d1) = (f a1, f b1, f c1, f d1)
 
 -- Split the timing data in intervals
 binTimingData :: Int -> V.Vector a -> [V.Vector a]
@@ -43,7 +80,7 @@ binTimingData binCount td = bin td
       where (f,r) = V.splitAt splitLength i
 
 createRandomData :: IO TimingData
-createRandomData = V.replicateM 100000 tdb
+createRandomData = V.replicateM 1000 tdb
   where rgen = randomRIO (0,1000.0)
         tdb :: IO TimingDataBlock
         tdb = (,,,) <$> rgen <*> rgen <*> rgen <*> rgen
